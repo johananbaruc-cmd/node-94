20cbd8fc4b2e623c85807f3394c439d27ddaeb66
Johanan Baruc Pineda <johananbaruc@gmail.com>
2026-05-20 19:59:38 -0600

    servidor 3000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.send("Hola");
+});
+
+app.listen(3000, () => { 
+    console.log("escuchando en el puerto 3000");
+})
